3b82064a7e9f76f28ba62a099ee09174581446ed
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-16 00:00:28 +0700

    update actionSignUp function

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -36,16 +36,71 @@ module.exports = {
          res.redirect('/auth/signup');
       }
    },
+
    actionSignUp: async(req, res) => {
       try {
          const { username, email, password, password2 } = req.body;
          console.log(
 		   	" Username " + username + " email :" + email + " pass:" + password
 		   );
+
+         //check if all input fields has filled 
          if(!username || !email || !password || !password2) {
             req.flash('alertMessage', 'Please Fill in all fields');
             req.flash('alertStatus', 'danger');
             res.redirect('/auth/signup');
+         }
+
+         //check if password match
+         else if(password !== password2) {
+            req.flash('alertMessage', "Passwords doesn't match");
+            req.flash('alertStatus', 'danger');
+            res.redirect('/auth/signup');
+         }
+
+         //check if password is more than 6 characters
+         else if(password.length < 6 ) {
+            req.flash('alertMessage', 'Password at least have 6 characters');
+            req.flash('alertStatus', 'danger');
+            res.redirect('/auth/signup');
+         }
+         else {
+            User.findOne({ username: username }).then((uname) => {
+               if(uname) {
+                  req.flash('alertMessage', 'username already registered');
+                  req.flash('alertStatus', 'danger');
+                  res.redirect('/auth/signup');
+               } else {
+                  User.findOne({ email: email }).then((mail) => {
+                     if (mail) {
+                        req.flash('alertMessage', 'email already registered');
+                        req.flash('alertStatus', 'danger');
+                        res.redirect('/auth/signup');
+                     } else {
+                        const newUser = new User({
+                           username : username,
+                           email : email,
+                           password : password
+                        });
+                        // hash password
+                        bcrypt.genSalt(10, (err, salt) => {
+                           bcrypt.hash(newUser.password, salt, (err, hash) => {
+                              if(err) throw(err);
+                              // save pass to hash
+                              newUser.password = hash;
+                              // save user
+                              newUser.save().then((value) => {
+                                 console.log(value);
+                                 req.flash('success_msg', 'You have now registered!');
+                                 res.redirect('/auth/login');
+                              })
+                              .catch(value=> console.log(value));
+                           })
+                        })
+                     }
+                  })
+               }
+            })
          }
       } catch (err) {
          req.flash('alertMessage', `${err.message}`);
--- app/auth/test.js
+++ app/auth/test.js
@@ -16,20 +16,67 @@ module.exports = {
       }
    },
    actionSignUp: async(req, res) => {
-      try {
-         const { username, email, password, password2 } = req.body;
-         console.log(
-		   	" Username " + username + " email :" + email + " pass:" + password
-		   );
+      const {username,email, password, password2} = req.body;
+         let errors = [];
+         console.log(' Username ' + username+ ' email :' + email+ ' pass:' + password);
+         
+         //check if all input fields has filled 
          if(!username || !email || !password || !password2) {
-            req.flash('alertMessage', 'Please Fill in all fields');
-            req.flash('alertStatus', 'danger');
-            res.redirect('/auth/signup');
+            errors.push({msg : "Please fill in all fields"})
          }
-      } catch (err) {
-         req.flash('alertMessage', `${err.message}`);
-         req.flash('alertStatus', 'danger');
-         res.redirect('/auth/signup');
-      }
+         //check if password match
+         if(password !== password2) {
+            errors.push({msg : "passwords doesn't match"});
+         }
+
+         //check if password is more than 6 characters
+         if(password.length < 6 ) {
+            errors.push({msg : 'password atleast 6 characters'})
+         }
+         if(errors.length > 0 ) {
+            res.render('signup', {
+               errors : errors,
+               username : username,
+               email : email,
+               password : password,
+               password2 : password2
+            })
+         } else {
+            //validation passed
+            User.findOne({ username: username }).then((uname) => {
+               if(uname) {
+                  errors.push({msg: 'username already registered'});
+                  res.render('signup',{errors,username,email,password,password2})
+               } else {
+                  User.findOne({ email: email }).then((mail) => {
+                     if (mail) {
+                        errors.push({msg: 'email already registered'});
+                        res.render('signup',{errors,username,email,password,password2})
+                     } else {
+                        const newUser = new User({
+                           username : username,
+                           email : email,
+                           password : password
+                        });
+                        // hash password
+                        bcrypt.genSalt(10, (err, salt) => {
+                           bcrypt.hash(newUser.password, salt, (err, hash) => {
+                              if(err) throw(err);
+                              // save pass to hash
+                              newUser.password = hash;
+                              // save user
+                              newUser.save().then((value) => {
+                                 console.log(value);
+                                 req.flash('success_msg', 'You have now registered!');
+                                 res.redirect('/auth/login');
+                              })
+                              .catch(value=> console.log(value));
+                           })
+                        })
+                     }
+                  })
+               }
+            })
+         }    
    },
 }
