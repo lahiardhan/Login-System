8e0ea991038333a53d311fafaa5be2d5e8864c06
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-16 19:54:50 +0700

    replace directory of middleware/auth

--- app/users/router.js
+++ app/users/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { index, dashboard } = require('./controller');
-const {ensureAuthenticated} = require("../../config/auth");
+const {ensureAuthenticated} = require("../middleware/auth");
 
 router.get('/', index);
 router.get('/dashboard', ensureAuthenticated, dashboard);
